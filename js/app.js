4049977fbbb92fb4c3d3aa24f1564636c103bbb6
Temiloluwa Jikiemi <temiloluwajik@gmail.com>
2023-08-02 14:47:09 -0700

    add dynamic feed generation

--- js/app.js
+++ js/app.js
@@ -1,9 +1,170 @@
-// define custom seraches
+//DOM nodes
 
-const any = 'q=fashion';
+const pageLinks = document.querySelectorAll(".nav .list_link"),
+    pages = document.querySelectorAll(".page");
+// previous and next buttons
+const prev = document.querySelector(".prev"),
+    next = document.querySelector(".next"),
+    more=document.querySelector(".more");
 
-async function getNews(search) {
-    let data = await fetch(`https://newsapi.org/v2/everything?${search}&apiKey=5dd14bd3df04419096998429a02b86fc&sortBy=popularity`)
+
+//add event listeners
+for (const link of pageLinks) {
+    link.addEventListener("click", changePage)
+}
+prev.addEventListener("click", changeFeed);
+next.addEventListener("click", changeFeed);
+more.addEventListener("click",  moreFeed);
+
+//search queries
+const today = new Date().toISOString(),
+    newsapi = `https://newsapi.org/v2/everthing?apiKey=5dd14bd3df04419096998429a02b86fc&language=en&from=${date}`,
+    tech = `${newsapi}&q=tech&category=technology`,
+    politics = `${newsapi}&q=politics`,
+    fashion = `${newsapi}&q=fashion`,
+    health = `${newsapi}&q=health`,
+    sports = `${newsapi}&q=sport`,
+    trade = `${newsapi}&q=business`,
+    any = `${newsapi}`;
+
+
+const NEWS_OBJECTS = [];
+
+// news objects
+class NewsSection {
+    constructor(ownQry, childQry, type = "home", url) {
+        this.node = document.querySelector(ownQry)
+        this.index = -1;
+        this.pageCoUnt = 1;
+        this.articles = [];
+        this.type = type;
+        this.url = url
+        this.childQry = childQry;
+        let thisObj = this;
+        NEWS_OBJECTS.push(thisObj)
+    }
+    async getArticles() {
+        this.articles.concat(getNews(`${url}&pageCount`).articles)
+    }
+
+    get children() {
+        return this.node.querySelectorAll(this.childQry)
+    }
+    setNewsNode(node) {
+        let headline, text, author, img, link;
+        if (headline = node.querySelector(".headline")) {
+            // headline.innerText = articles[this.index][]
+        }
+        if (text = node.querySelector(".news_text")) {
+            // text.innerText = articles[this.index][]
+        }
+        if (img = node.querySelector(".news_photo")) {
+            // img.src = article[this.index][]
+        }
+        if (author = node.querySelector(".author")) {
+            // author.innerText = articles[this.index][]
+        }
+        if (link = node.querySelector("a")) {
+            // link.href = articles[this.index][]
+        }
+
+    }
+}
+
+class OtherNewsSection extends NewsSection {
+    constructor(ownQry, childQry, type, url) {
+        super(ownQry, childQry, type, url)
+    }
+    updateFeeds() {
+
+        console.log("updating feed of other news section");
+        let count, add;
+        if (this.index > -1 && this.children.length == 0) {
+            count = this.index;
+            add = 0;
+        } else { count = 12;
+        add=1 }
+
+        console.log(count,add)
+        for (let i = 0; i < count; i++) {
+            //Create a news article
+            let article = document.createElement("article"),
+                row = document.createElement("div"),
+                img = document.createElement("img"),
+                news_text_col = document.createElement("div"),
+                link = document.createElement("a"),
+                h2 = document.createElement("h2"),
+                p = document.createElement("p"),
+                author = document.createElement("span");
+
+            article.classList += "news_box col-lg-4";
+            row.classList += "row";
+            img.classList += "col-3 col-lg-7 news_photo";
+            news_text_col.classList += "news_texts_col col-9 col-lg-11";
+            h2.classList += "headline";
+            p.classList += "news_text";
+            author.classList += "author";
+
+            link.appendChild(h2);
+            news_text_col.appendChild(link);
+            news_text_col.appendChild(p);
+            news_text_col.appendChild(author);
+            row.appendChild(img);
+            row.appendChild(news_text_col);
+            article.appendChild(row);
+
+            this.setNewsNode(article);
+            this.node.appendChild(article)
+
+            this.index +=add
+        }
+    }
+}
+
+const home_feed_row = new NewsSection("#home_page .feed_row", ".feed_row .news_box", "home", any);
+home_feed_row.updateFeeds = function (dir = next) {
+    //if  disable previous button 
+    let count = (dir == "next") ? 1 : -1;
+    //if to disable next button
+
+    //make feeds fade out
+    this.node.classList.toggle("hide")
+    // set feeds
+    for (const node of this.children) {
+        this.setNewsNode(node);
+        this.index += count;
+    }
+
+    if (this.index < this.children.length) {
+        prev.disabled = true
+    } else {
+        prev.disabled = false
+    }
+    if (this.index >= 100) {
+        next.disabled = true
+    } else {
+        next.disabled = false
+    }
+    //fade-in
+    setTimeout(() => { this.node.classList.toggle("hide") }, 2000)
+}
+home_feed_row.updateFeeds()
+
+//define other news section objects
+let tech_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "tech", tech),
+    politics_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "politics", politics),
+    fashion_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "fashion", fashion),
+    health_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "health", health),
+    sport_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "sports", sports),
+    trade_feed_row = new OtherNewsSection("#other_page .feed_row", ".feed_row .news_box", "trade", trade);
+
+
+
+
+
+
+async function getNews(url) {
+    let data = await fetch(url)
         .then(res => res.json())
         .then(data => data)
         .catch(err => {
@@ -12,84 +173,55 @@ async function getNews(search) {
     return data;
 }
 
-async function setNews(node, data) {
-    // grab each article of the home page
+//EVENT LISTENERS
+//change pages
+let current="home"
+function changePage(event) {
+    // change page
+    event.preventDefault();
+    event.stopPropagation();
+    let type = event.target.innerText.toLowerCase()
 
-    if (node.nodeName == "IMG") {
-        node.src = "";
-    } else if (node.nodeName == "A") {
-        node.href = data;
-    } else {
-        node.innerText = data;
+    if (type == "home") {
+        pages.forEach(page => {
+            if (page.id == "home_page") {
+                page.hidden = false
+            } else {
+                page.hidden = true
+            }
+        })
+    } else if (type == "") { }
+    else {
+        pages.forEach(page => {
+            if (page.id == "other_page") {
+                page.querySelector(".feed_row").innerHTML = ""
+                NEWS_OBJECTS.forEach((obj) => {
+                    if (obj.type == type) {
+                        current = obj.type
+                        console.log(`paged changed to ${obj.type}`, obj.children.length)
+                        obj.updateFeeds()
+                    }
+                })
+                page.hidden = false;
+            } else {
+                page.hidden = true
+            }
+        })
     }
-    console.log(node.nodeName)
+    // generate feeds
 }
 
-// for each node under news update
-async function updateNodes(nodeSel) {
-    try {
-        let { articles } = await getNews("q=tech"),
-            newsNode = document.querySelectorAll(`${nodeSel}`),
-            headline, news_text, category, news_photo, link, i = 0;
-
-        console.log(articles, newsNode)
-
-        for (node of newsNode) {
-            // set each childs content
-            if (headline = node.querySelector(".headline")) {
-                setNews(headline, articles[i].title)
-            }
-            if (news_text = node.querySelector(".news_text")) {
-                setNews(news_text, articles[i].description.slice(0, 150).concat("..."))
-            }
-            if (category = node.querySelector(".category")) {
-                setNews(category, articles[i].author)
-            }
-            if (news_photo = node.querySelector(".news_photo")) {
-                setNews(news_photo, articles[i].urlToImage)
-            }
-            if (link = node.querySelector("a")) {
-                setNews(link, articles[i].url)
-            }
-            i++;
-        }
-    } catch (err) { console.error(err) }
-
-}
-
-// updateNodes('.feeds .news_box');
-
-
-
-// paginate feeds
-let feed_row = {
-    articles: document.querySelectorAll(".feed_row .news_box"),
-    index: 0,
-    updateFeeds(dir) {
-        let children = this.node.querySelectorAll(".news_box");
-        
-        //if  disable previous button
-        
-        //if to disable next button
-        
-        //make feeds fade out
-        this.node.classList.toggle("hide")
-        
-        // change feeds
-        
-
-        //fade-in
-        setTimeout(() => { this.node.classList.toggle("hide") }, 2500)
-    },
-    node: document.querySelector(".feed_row"),
-}
-
+// update home feeds
 function changeFeed(event) {
-    feed_row.updateFeeds(event.target.value.toLowerCase());
+    home_feed_row.updateFeeds(event.target.value.toLowerCase());
 }
-// previous and next buttons
-const prev = document.querySelector(".prev"),
-next = document.querySelector(".next");
 
-prev.addEventListener("click", changeFeed);
-next.addEventListener("click", changeFeed);
+function moreFeed(event){
+    NEWS_OBJECTS.forEach((obj) => {
+        if (obj.type == current) {
+            obj.updateFeeds()
+            
+            console.log(`added feed to ${obj.type}`, obj.children.length)
+        }
+    })
+}
